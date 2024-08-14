7c0c2a9f117e1b7eac8c9be8e3ec04ae3f3c7420
Dmytro Cheva <dmytro.cheva@gmail.com>
2024-08-14 13:02:43 +0300

    bucket

--- Player.gd
+++ Player.gd
@@ -1,54 +1,68 @@
 extends KinematicBody2D
 
 var screen_size
+var velocity = Vector2()
 var steer = 0
 var speed = 0
 var rot_speed = 0.15
+var max_steer = 10
 var opt_speed = 30
 var max_speed = 300
-var max_steer = 60
-var velocity = Vector2()
+var min_speed = 10
 
 func _ready():
 	screen_size = get_viewport_rect().size
-	
-func get_input():
-	var speed_to = speed 
-	var steer_to = steer
-	if Input.is_action_pressed("space"):
-		speed_to = lerp(speed_to, speed_to/2, 0.2)
-	if Input.is_action_pressed("right_arrow"):
-		if speed > 0:
-			steer_to += 1
-		else:
-			steer_to -= 1
-	if Input.is_action_pressed("left_arrow"):
-		if speed > 0:
-			steer_to -= 1
-		else:
-			steer_to += 1
-	if Input.is_action_pressed("up_arrow"):
-		speed_to += max_speed / 20
-	if Input.is_action_pressed("down_arrow"):
-		speed_to -= max_speed / 50
-	
-	# Physics Processing: steering with speed
-	if speed_to != 0 and abs(speed_to) < opt_speed:
-		steer_to = steer_to * ((abs(speed_to) + opt_speed) / (2 * opt_speed))
-	elif speed_to != 0:
-		steer_to = steer_to * (max_speed - sqrt(abs(speed_to))) / max_speed
-	
-	# Physics Processing: LERP
-	speed = lerp(speed_to, speed/2, 0.02)
-	steer = lerp(steer_to, steer/2, 0.1)
-	
-	# Physics Processing: CLAMP
-	speed = clamp(speed, -max_speed/2, max_speed)
-	steer  = clamp(steer, -max_steer, max_steer)
-	
 	
 func _physics_process(delta):
 	get_input()
 	rotation += steer * rot_speed * delta
 	velocity = Vector2(0, -speed).rotated(rotation)
 	velocity = move_and_slide(velocity)
+	
+func get_input():
+	var speed_to = 0
+	var steer_to = steer
+	
+	if Input.is_action_pressed("right_arrow"):
+		steer_to = max_steer
+	if Input.is_action_pressed("left_arrow"):
+		steer_to = -max_steer
+	if Input.is_action_pressed("up_arrow"):
+		speed_to = max_speed
+	if Input.is_action_pressed("down_arrow"):
+		speed_to = -max_speed/2
+	if Input.is_action_pressed("space"):
+		get_drift()
+		
+	get_physics(speed_to, steer_to)
+
+func get_drift():
+	speed = lerp(speed, 0, 0.1)
+
+func get_physics(speed_to, steer_to):
+	
+	# Reverse steering
+	if speed < 0: 
+		steer_to = -steer_to
+
+	# Physics with LERP
+	speed = lerp(speed, speed_to, 0.02)
+	steer = lerp(steer, steer_to, 0.1)
+
+	# Speed ​​steering
+	if speed > 0:
+		if abs(speed) < opt_speed:
+			steer = steer * ((abs(speed) + opt_speed) / (2 * opt_speed))
+		if abs(speed) > opt_speed:
+			steer = steer * (max_speed - sqrt(abs(speed))) / max_speed
+
+	# Speed limits
+	steer  = clamp(steer, -max_steer, max_steer)
+	speed = clamp(speed, -max_speed/2, max_speed)
+
+	# Autobrake
+	if abs(speed) < min_speed:
+		steer = 0
+		if speed_to == 0:
+			speed = 0
+
